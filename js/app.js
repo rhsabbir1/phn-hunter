a1d84e028430a065cf24fd6d6fb8fd6141fe5532
rhsabbir1 <rhsabbir090@gmai.com>
2023-02-26 17:15:42 +0600

    show data in modal

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
-const loadPhones = (searchText) => {
+const loadPhones = (searchText , datalimit) => {
     const URL2 = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(URL2)
         .then(res => res.json())
-        .then(data => displayPhones(data.data))
+        .then(data => displayPhones(data.data , datalimit))
 }
-const displayPhones = phones => {
+const displayPhones = (phones , datalimit) => {
     const cardContainer = document.getElementById('card-container')
     cardContainer.innerHTML = '';
-    phones = phones.slice(0, 20)
+    
+    const showAll = document.getElementById('show-all-text');
+
+    if( datalimit && phones.length > 10){
+        phones = phones.slice(0, 10)
+        showAll.classList.remove('d-none')
+    }
+    else{
+        showAll.classList.add('d-none')
+    }
+
+
     const wearningText = document.getElementById('worning')
 
     if (phones.length == 0) {
@@ -17,7 +28,7 @@ const displayPhones = phones => {
         wearningText.classList.add('d-none')
     }
     for (const phone of phones) {
-        const {  phone_name, image } = phone;
+        const {  phone_name, image,slug } = phone;
         const ceratDiv = document.createElement('div')
         ceratDiv.classList.add('col')
         ceratDiv.innerHTML = `
@@ -28,6 +39,7 @@ const displayPhones = phones => {
                         <p class="card-text">This is a longer card with supporting text below as a natural lead-in
                         to additional content. This content is a little bit longer.</p>
                 </div>
+                <div onclick=showDetails('${slug}') id="btn-show-all" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneModal">Show Details </div>
           </div>
         `;
         cardContainer.appendChild(ceratDiv)
@@ -35,13 +47,21 @@ const displayPhones = phones => {
     // stop loading 
     loading(false)
 }
+const processSearch =(datalimit) =>{
+    loading(true)
+    const searchText = document.getElementById('search-field').value;
+    loadPhones(searchText,datalimit)
+}
 
 document.getElementById('search-phone').addEventListener('click', function () {
     // start loading 
-    loading(true)
-    const searchText = document.getElementById('search-field').value;
-    loadPhones(searchText)
+    processSearch(10)
 })
+document.getElementById('search-field').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        processSearch(10)
+    }
+});
 
 const loading = (isloading) => {
     const loaderSection = document.getElementById('loader')
@@ -53,8 +73,28 @@ const loading = (isloading) => {
     }
 }
 
-loadPhones('phone');
+document.getElementById('btn-show-all').addEventListener('click' , function(){
+    processSearch();
+})
+
+const showDetails =(id)=>{
+    fetch(` https://openapi.programming-hero.com/api/phone/${id}`)
+    .then(res => res.json())
+    .then(data => showModalData(data.data))
+}
+
+const showModalData =(data) => {
+    console.log(data)
+    const phnModalTItle = document.getElementById('phnModalTItle');
+    phnModalTItle.innerText = data.name;
+    const phnDetails = document.getElementById('phn-details');
+    phnDetails.innerHTML=`
+    <img src="${data.image}" alt="">
+    <p>Release Date : ${data.releaseDate ? data.releaseDate : 'No resutl' }</p>
+    `
+}
+// loadPhones();
 
 // git add .
-// git commit -m"Add spinner "
+// git commit -m"show data in modal"
 // git push
