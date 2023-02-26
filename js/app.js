1218823421f0675bec9df814824313c6bffe2311
rhsabbir1 <rhsabbir090@gmai.com>
2023-02-26 12:33:19 +0600

    show search phn

--- js/app.js
+++ js/app.js
@@ -1,31 +1,40 @@
-const loadPhones = () => {
-    const URL = `https://openapi.programming-hero.com/api/phones?search=iphone`;
-    fetch(URL)
+const loadPhones = (searchText) => {
+        const URL2 = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    // const URL = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    fetch(URL2)
         .then(res => res.json())
         .then(data => displayPhones(data.data))
 }
 const displayPhones = phones => {
     const cardContainer = document.getElementById('card-container')
-
+    cardContainer.innerHTML = '';
+    phones = phones.slice(0 , 20)
     for (const phone of phones) {
         const {brand ,phone_name,image,slug} = phone;
-        console.log(phone)
         const ceratDiv = document.createElement('div')
         ceratDiv.classList.add('col')
         ceratDiv.innerHTML = `
-        <div class="card">
-                        <img src="${image}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">${phone_name}</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                                to additional content. This content is a little bit longer.</p>
-                        </div>
-                    </div>
+        <div class="card p-4">
+                <img src="${image}" class="card-img-top" alt="...">
+                 <div class="card-body">
+                    <h5 class="card-title">${phone_name}</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in
+                        to additional content. This content is a little bit longer.</p>
+                </div>
+          </div>
         `;
         cardContainer.appendChild(ceratDiv)
     }
 }
 
+document.getElementById('search-phone').addEventListener('click' , function(){
+    const searchText = document.getElementById('search-field').value;
+    loadPhones(searchText)
+})
+
+
 loadPhones();
 
 git add .
+git commit -m"show search phn "
+git push
